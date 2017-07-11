e28c470f2de14499958ae8b6fb49bb8ddb231f4c
nazuna <nazuna@nazuna.kr>
2017-07-11 16:44:55 +0900

    LoginFrm 디자인 구현

--- Client/uLogin.pas
+++ Client/uLogin.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.StdCtrls, Vcl.ExtCtrls,
+  Vcl.Imaging.pngimage, Vcl.Buttons;
 
 type
   TLoginFrm = class(TForm)
+    Image1: TImage;
+    edtPC: TEdit;
+    Edit2: TEdit;
+    SpeedButton1: TSpeedButton;
+    SpeedButton2: TSpeedButton;
+    procedure Button1Click(Sender: TObject);
+    procedure FormCanResize(Sender: TObject; var NewWidth, NewHeight: Integer;
+      var Resize: Boolean);
   private
     { Private declarations }
   public
@@ -20,5 +29,16 @@ var
 implementation
 
 {$R *.dfm}
+
+procedure TLoginFrm.Button1Click(Sender: TObject);
+begin
+loginfrm.Close;
+end;
+
+procedure TLoginFrm.FormCanResize(Sender: TObject; var NewWidth,
+  NewHeight: Integer; var Resize: Boolean);
+begin
+  Resize:=false;
+end;
 
 end.
